8694444fe3b477ea95b8d030ccbca9d0261bf103
xiucai <xiucai@pahuashan.cn>
2017-06-07 12:15:35 +0800

    add cursor styles

--- CFGUI/cfg.js
+++ CFGUI/cfg.js
@@ -9,7 +9,7 @@ var cfg = {
     disk: {},
     netcard: {},
     router: {},
-    switch: {},
+    switcher: {},
     firewall: {},
     template: [
         {name:"",feature:[]},
@@ -25,6 +25,8 @@ var message = document.getElementById("message");
 var state = document.getElementById("state");
 var owner = document.getElementById("owner");
 var stamp = document.getElementById("stamp");
+var paper = document.getElementById("paper");
+var pc = document.getElementById("paper-curosr");
 
 function id2object(_) {
     if (typeof(_) != "object") {
@@ -35,19 +37,35 @@ function id2object(_) {
 function shown(x) {
     id2object(x).setAttribute("style", "display:inherit;");
 }
-function hiden(x) {
+function hidden(x) {
     id2object(x).setAttribute("style", "display:none;");
 }
 function toggle(x) {
     var _ = id2object(x);
     var __ = _.getAttribute("soh");
     if (__ == "shown") {
-        hiden(_);
-        _.setAttribute("soh", "hiden");
+        hidden(_);
+        _.setAttribute("soh", "hidden");
     }
     else {
         shown(_);
         _.setAttribute("soh", "shown");
+    }
+}
+function sign(what, checked, unchecked) {
+    var lis = what.parentNode.getElementsByTagName("li");
+    for (var n=0; n<lis.length; n++) {
+        if (lis[n]!=what) {
+            lis[n].setAttribute("state", "unchecked");
+        }
+        lis[n].setAttribute("style", unchecked || "background-color:white;color:black;");
+    }
+    if (what.getAttribute("state") == "unchecked") {
+        what.setAttribute("state", "checked");
+        what.setAttribute("style", checked || "background-color:green;color:white;");
+    }
+    else {
+        what.setAttribute("state", "unchecked");
     }
 }
 
@@ -60,6 +78,58 @@ function initialize() {
 
 function dispatch(what) {
     var todo = what.innerHTML;
+    sign(what);
+}
+
+function select(what) {
+    sign(what);
+}
+
+function mode(m) {
+    // TODO: change class instead of changing style to avoid cursor-*'s repeated loading
+    var cursor = "{cursor:move;}";
+    switch(m) {
+        case "mainboard": {
+            cursor = "{cursor:url('./cursor-mainboard.png'),default;}";
+            break;
+        }
+        case "cpu": {
+            cursor = "{cursor:url('./cursor-cpu.png'),default;}";
+            break;
+        }
+        case "gpu": {
+            cursor = "{cursor:url('./cursor-gpu.png'),default;}";
+            break;
+        }
+        case "memory": {
+            cursor = "{cursor:url('./cursor-memory.png'),default;}";
+            break;
+        }
+        case "disk": {
+            cursor = "{cursor:url('./cursor-disk.png'),default;}";
+            break;
+        }
+        case "netcard": {
+            cursor = "{cursor:url('./cursor-netcard.png'),default;}";
+            break;
+        }
+        case "router": {
+            cursor = "{cursor:url('./cursor-router.png'),default;}";
+            break;
+        }
+        case "switcher": {
+            cursor = "{cursor:url('./cursor-switcher.png'),default;}";
+            break;
+        }
+        case "firewall": {
+            cursor = "{cursor:url('./cursor-firewall.png'),default;}";
+            break;
+        }
+        default: {
+            break;
+        }
+    }
+    pc.innerHTML = ".canvas:hover" + cursor;
 }
 
 function WhatIsTheNext() {
--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # howtojs
 HV2D: a rough 2d game sample of html5 based on nx2d.js  
-CFGUI: a graphics user interface for configuring somethings like libvirt, openstack, ceph and so on.  
+CFGUI: a graphics user interface for configuring somethings like libvirt, openstack, docker and so on.  
 
 # contact
 me@nagexiucai.com
