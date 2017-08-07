2b2377ed67b343732a091a45a412e34808b2d73c
bob-nx <me@nagexiucai.com>
2017-08-07 10:17:31 +0800

    add pure css-iii timeline example

---
+++ utils/clipboard.js
@@ -0,0 +1,31 @@
+// https://github.com/nagexiucai/howtojs/utils
+
+
+function chainMaker (c) {
+  // todo: make websocket
+  if (typeof c == "string" and c.startsWith("ws://")) {
+
+  }
+  else {
+    return c;
+  }
+}
+
+!function (object, name, version, chain){
+  var o = object;
+  var name = name;
+  var version = version;
+  var chain = chain;
+
+  var onCopy = function (evt) {
+
+  };
+  var onPaste = function (evt) {
+
+  };
+  var onDrag = function (evt) {};
+  var onDrop = function (evt) {};
+
+  o.addEventListener("copy", onCopy);
+  o.addEventListener("paste", onPaste);
+}(document, "clipboard", "0.0.1", rbf);
