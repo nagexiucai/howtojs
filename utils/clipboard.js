bd69fd98413a882c6d0058112e2a80f0cadd8b30
bob-nx <me@nagexiucai.com>
2017-08-23 18:29:57 +0800

    update clipboard.js in utils

--- utils/clipboard.js
+++ utils/clipboard.js
@@ -29,3 +29,26 @@ function chainMaker (c) {
   o.addEventListener("copy", onCopy);
   o.addEventListener("paste", onPaste);
 }(document, "clipboard", "0.0.1", rbf);
+
+/**
+ * Coding:UTF-8
+ * Clipboard upon noVNC.
+ * Bob·NX
+ *
+ * usage:
+ * <script type="text/javascript" src="path-contains-me/clipboard.js"></script>
+ * <textarea id="clipboard" placeholder="请将要传送到后台的代码文本粘贴于此" maxlength="1024" style="fix-me"></textarea>
+ */
+
+function _clipboard_(d) {
+    var me = d.getElementById("clipboard");
+    me.addEventListener("paste", function (evt) {
+        var text = evt.clipboardData.getData("text/plain");
+        // TODO: 传送两次降低失败概率
+        d.rfb.clipboardPasteFrom(text);
+        d.rfb.clipboardPasteFrom(text);
+    });
+    d.rfb._onClipboard = function (__, text) {
+        me.value = text;
+    };
+}(document);
